63f35691413b6dc8ac3a279fd189014b99092116
Gilad Goldberg <giladgo@gmail.com>
2015-01-18 11:26:12 +0200

    Fixing some mistakes; config doesn't work yet :(

--- cloud/update_songs.js
+++ cloud/update_songs.js
@@ -10,30 +10,38 @@ Parse.Cloud.job("update_songs", function(request, status) {
     _.each(users, function(user) {
       promise = promise.then(function() {
         return getSongByDay(user).then(function(song){
-          console.log("song change for " + user.get('username') + ": " + song.get('artist_name') + " " + song.get('song_name'));
-          return Parse.Cloud.httpRequest({
-            method: 'POST',
-            url: "https://api.parse.com/1/functions/changeSong",
-            headers: {
-              "X-Parse-Application-Id": Parse.Config.get('nomex_application_id'),
-              "X-Parse-REST-API-Key": Parse.Config.get('nomex_api_key'),
-              "Content-Type": "application/json"
-            },
-            body: {
-              "token": user.get('nomex_id'),
-              "song": "SONGID", // ??? 
-              "name": song.get('song_name'),
-              "artist": song.get('artist_name'),
-              "youtubeId": song.get('youtube_id'),
-              "startAtInSeconds": song.get('second_offset')
-            }
+          var parseConfig = Parse.Config.get().then(function(parseConfig) {
+            console.log('zomg zomg');
+            return Parse.Cloud.httpRequest({
+              method: 'POST',
+              url: "https://api.parse.com/1/functions/changeSong",
+              headers: {
+                "X-Parse-Application-Id": parseConfig.get('nomex_application_id'),
+                "X-Parse-REST-API-Key": parseConfig.get('nomex_api_key'),
+                'Content-Type': 'application/json;charset=utf-8'
+              },
+              body: {
+                "token": user.get('nomex_id'),
+                "song": user.get('song_id'),
+                "name": song.get('song_name'),
+                "artist": song.get('artist_name'),
+                "youtubeId": song.get('youtube_id'),
+                "startAtInSeconds": song.get('second_offset')
+              }
+            }).then(function(result) {
+              return Promise.as({
+                'username': user.get('username'),
+                'artist_name': song.get('artist_name'),
+                'song_name': song.get('song_name')
+              });
+            });
           });
         });
       });
     });
     return promise;
-  }).then(function() {
-      status.success("awesome");
+  }).then(function(result) {
+      status.success(result);
     }, function(err) {
       console.log(err);
       status.error(err.message);
