7f4717c2c85c8c471f300ff2432533e1b9fae323
Gilad Goldberg <giladgo@gmail.com>
2015-01-20 10:42:53 +0200

    Fixing job, it works

--- cloud/update_songs.js
+++ cloud/update_songs.js
@@ -1,17 +1,20 @@
 var _ = require('underscore');
 
+function getParseConfig() {
+  return Parse.Config.get();
+}
+
 Parse.Cloud.job("update_songs", function(request, status) {
   // Set up to modify user data
   Parse.Cloud.useMasterKey();
-    
+
   var query = new Parse.Query(Parse.User);
   return query.find().then(function(users) {
     var promise = Parse.Promise.as();
     _.each(users, function(user) {
       promise = promise.then(function() {
         return getSongByDay(user).then(function(song){
-          var parseConfig = Parse.Config.get().then(function(parseConfig) {
-            console.log('zomg zomg');
+          return Parse.Config.get().then(function(parseConfig) {
             return Parse.Cloud.httpRequest({
               method: 'POST',
               url: "https://api.parse.com/1/functions/changeSong",
@@ -26,24 +29,17 @@ Parse.Cloud.job("update_songs", function(request, status) {
                 "name": song.get('song_name'),
                 "artist": song.get('artist_name'),
                 "youtubeId": song.get('youtube_id'),
-                "startAtInSeconds": song.get('second_offset')
+                "startAtInSeconds": song.get('second_offset')*1
               }
-            }).then(function(result) {
-              return Promise.as({
-                'username': user.get('username'),
-                'artist_name': song.get('artist_name'),
-                'song_name': song.get('song_name')
-              });
             });
           });
         });
       });
     });
     return promise;
-  }).then(function(result) {
-      status.success(result);
+  }).then(function() {
+      status.success("Cool.");
     }, function(err) {
-      console.log(err);
       status.error(err.message);
     }
   );
